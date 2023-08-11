ecee55a599991cd6dc25b9179ee506edb28472e0
Abhishek Kumar <akbittu@gmail.com>
2023-08-12 00:42:10 +0530

    Created the app

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+// svelte.config.js
+export default {
+  // svelte options
+  extensions: ['.svelte'],
+  compilerOptions: {},
+  preprocess: [],
+  onwarn: (warning, handler) => handler(warning),
+  // plugin options
+  vitePlugin: {
+    exclude: [],
+    // experimental options
+    experimental: {}
+  }
+};
